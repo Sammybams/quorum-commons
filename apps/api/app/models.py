d2bd780bc204dc08ed40f3de32ce171b8176aab1
Sammybams <samuelbamgbola@gmail.com>
2026-05-12 12:21:22 +0100

    feat: import api data models

---
+++ apps/api/app/models.py
@@ -0,0 +1,33 @@
+from .database import Doc
+
+
+Workspace = Doc
+User = Doc
+Role = Doc
+WorkspaceMember = Doc
+Integration = Doc
+Invitation = Doc
+InviteLink = Doc
+Member = Doc
+DuesCycle = Doc
+DuesPayment = Doc
+Event = Doc
+Campaign = Doc
+FundingStream = Doc
+Contribution = Doc
+ShortLink = Doc
+Announcement = Doc
+AuthSession = Doc
+RevokedToken = Doc
+EmailVerificationToken = Doc
+PasswordResetToken = Doc
+EventAttendee = Doc
+Meeting = Doc
+MeetingMinutes = Doc
+ActionItem = Doc
+Task = Doc
+Budget = Doc
+BudgetLine = Doc
+Expenditure = Doc
+Notification = Doc
+Report = Doc
